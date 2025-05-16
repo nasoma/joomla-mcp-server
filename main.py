6ef239dfab34bd4d0e9cbe30dabcd51f02b136f8
Antole <pythonantole@gmail.com>
2025-05-16 18:45:10 +0300

    updated port for Render web services

--- main.py
+++ main.py
@@ -6,9 +6,7 @@ from mcp.server.fastmcp import FastMCP
 import markdown
 import bleach
 
-mcp = FastMCP(
-    "Joomla Articles MCP",
-)
+mcp = FastMCP("Joomla Articles MCP", port=10000)
 
 JOOMLA_BASE_URL = os.getenv("JOOMLA_BASE_URL").rstrip("/")
 BEARER_TOKEN = os.getenv("BEARER_TOKEN")
@@ -444,4 +442,4 @@ async def update_article(
 
 
 if __name__ == "__main__":
-    mcp.run(transport="stdio")
+    mcp.run(transport="sse")
